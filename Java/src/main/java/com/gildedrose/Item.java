b77c77efec2f5d411b2f64d26e3d2b79d552a274
sfarah1 <sadieq.ai.f@gmail.com>
2024-06-25 13:25:37 +0100

    Add getters and setters to Item class

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,12 +1,9 @@
 package com.gildedrose;
 
 public class Item {
-
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,8 +11,33 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
+
