ec586f50158f456f89f187a1e0c08f8c2d57fefa
sfarah1 <sadieq.ai.f@gmail.com>
2024-06-25 18:47:54 +0100

    Revert changes to Item class to comply with requirements
    The Item class was reverted to its original state to comply with the project's requirements, which specify that the Item class and its properties should not be altered. The changes made included the removal of getter and setter methods that were previously added. This ensures that the Item class remains as it was originally defined, preventing any potential issues with shared code ownership as indicated in the specification.

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,37 +1,16 @@
 package com.gildedrose;
 
 public class Item {
-    private String name;
-    private int sellIn;
-    private int quality;
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSellIn() {
-        return sellIn;
-    }
-
-    public void setSellIn(int sellIn) {
-        this.sellIn = sellIn;
-    }
-
-    public int getQuality() {
-        return quality;
-    }
-
-    public void setQuality(int quality) {
         this.quality = quality;
     }
 
@@ -40,4 +19,3 @@ public class Item {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
-
